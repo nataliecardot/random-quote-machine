7ac229762b2a24f40c83cb525e4c8b9bf062954a
Natalie-Cardot <nataliecardot@gmail.com>
2019-07-07 21:28:51 -0700

    Fetch quotes, add method to retrieve random quote index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { random } from 'lodash';
 import Button from './components/Button';
 
 class App extends Component {
@@ -7,29 +8,32 @@ class App extends Component {
     super(props);
     this.state = {
       quotes: [],
-      selectedQuoteIndex: null
+      randomQuoteIndex: null
     }
+    this.randomQuoteIndex = this.randomQuoteIndex.bind(this);
   }
 
   componentDidMount() {
     // fetch method uses GET method by default; don't need to specify
-    fetch('https://gist.githubusercontent.com/christianvuerings/6624542/raw/379a1943f1881b3d3d4ca3e3f456df4d4608c9e7/quotes.json')
+    fetch('https://gist.githubusercontent.com/nataliecardot/0ca0878d2f0c4210e2ed87a5f6947ec7/raw/1802a693d02ea086817e46a42413c0df4c077e3b/quotes.json')
       // Takes a JSON response string and parses it into JS object
       .then(response => response.json())
-      .then(quotes => this.setState({ quotes }));
+      // state is set to quotes: quotes due to destructuring
+      .then(quotes => this.setState({ quotes }, () => {
+        this.setState({ randomQuoteIndex: this.randomQuoteIndex() })
+      }));
   }
 
-  nextQuoteClickHandler() {
-    console.log('Hello');
+  randomQuoteIndex() {
+    return random(0, this.state.quotes.length - 1);
   }
 
   render() {
-    console.log(this.state.quotes)
     return (
       <div className="App" id="quote-box">
         <Button
           buttonDisplayName="Next"
-          clickHandler={this.nextQuoteClickHandler}
+          clickHandler={this.randomQuoteIndex}
         />
       </div>
     );
