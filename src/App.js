9255b5429026851a5af693505ef140814f51cbce
Natalie-Cardot <nataliecardot@gmail.com>
2019-07-07 20:26:15 -0700

    Add comments

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import './App.css';
 import Button from './components/Button';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null
+    }
+  }
+
+  componentDidMount() {
+    // fetch method uses GET method by default; don't need to specify
+    fetch('https://gist.githubusercontent.com/christianvuerings/6624542/raw/379a1943f1881b3d3d4ca3e3f456df4d4608c9e7/quotes.json')
+      // Takes a JSON response string and parses it into JS object
+      .then(response => response.json())
+      .then(quotes => this.setState({ quotes }));
+  }
+
   nextQuoteClickHandler() {
     console.log('Hello');
   }
 
   render() {
+    console.log(this.state.quotes)
     return (
       <div className="App" id="quote-box">
         <Button
