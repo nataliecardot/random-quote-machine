419ee1e119de94648f26d8adc0272f92135eae1e
Natalie-Cardot <nataliecardot@gmail.com>
2019-07-10 13:56:26 -0700

    Integrate Material UI Card and Button APIs, remove old Button component

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import Button from './Button';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 // Parentheses around function body is implicit return
 const QuoteMachine = (props) => (
-  <>
-    {props.isDoneFetching ?
-      (
-        <Typography>
-          {props.randomQuote().quote} –{props.randomQuote().author}
-        </Typography>
-      ) : 'Loading...'}
+  <Card>
+    <CardContent>
+      {props.isDoneFetching ?
+        (
+          <Typography>
+            {props.randomQuote().quote} –{props.randomQuote().author}
+          </Typography>
+        ) : 'Loading...'}
+    </CardContent>
 
-    <Button
-      buttonDisplayName="Next"
-      clickHandler={props.nextRandomQuote}
-    />
-  </>
+    <CardActions>
+      <Button
+        size="large"
+        onClick={props.nextRandomQuote}
+      >
+        Next
+      </Button>
+    </CardActions>
+  </Card>
 )
 
 export default QuoteMachine;
